e3f0903159b5634f4e46e3f505fbe661b7c2ec45
Alvaro <Roekan03@gmail.com>
2023-08-01 12:53:36 +0200

    create styles of Magic card with bootstrap and customs css

--- src/common/magicCard/MagicCard.jsx
+++ src/common/magicCard/MagicCard.jsx
@@ -1,34 +1,64 @@
 import React from 'react'
-import { Button, Card } from 'react-bootstrap'
-
+import { Button, Card, Container, Row, Col } from 'react-bootstrap'
+import './MagicCard.css'
 
 export const MagicCard = ({name, colors, type, image}) => {
   return (
     <>
-      <Card style={{ width: '18rem' }}>
-
-        {image
-        ? <Card.Img variant="top" src={image} />
-        : <Card.Img variant="top" src={'./../../public/images/no-image.jpg'} />
-        }
-
-      <Card.Body>
-        <Card.Title>{name}</Card.Title>
-            {
-            colors.map((color, i)=>{
-                return (
-                    <Card.Text key={i} >
-                    {color}
+    <Card style={{ width: '18rem' }}>
+        <Container>
+            <Row className='d-flex align-items-top justify-content-between mt-2'>
+                <Col className='d-flex align-items-top justify-content-start' sm={4}>
+                    {image
+                    ? <Card.Img variant="top" src={image} />
+                    : <Card.Img variant="top" src={'./../../public/images/no-image.jpg'} />
+                    }
+                </Col>
+                <Col sm={8}>
+                        <Col sm={12}><Card.Title className='title-magiccard'>{name}</Card.Title></Col>
+                        <Col sm={12}><Card.Text className='type-magiccard'>{type}</Card.Text></Col>
+                        <Col sm={12}>
+                        {
+                colors.map((color, i)=>{
+                    return (
+                        <>
+                        {
+                        color == 'W' && <img className='img-color-magiccard' key={i} src="./../../public/images/elements/white.png" /> 
+                        }
+                        {
+                        color == 'U' && <img className='img-color-magiccard' key={i} src="./../../public/images/elements/blue.png" /> 
+                        }
+                        {
+                        color == 'G' && <img className='img-color-magiccard' key={i} src="./../../public/images/elements/green.png" /> 
+                        }
+                        {
+                        color == 'B' && <img className='img-color-magiccard' key={i} src="./../../public/images/elements/black.png" /> 
+                        }
+                        {
+                        color == 'R' && <img className='img-color-magiccard' key={i} src="./../../public/images/elements/red.png" /> 
+                        }
+                        {
+                        color == '' && <img className='img-color-magiccard' key={i} src="./../../public/images/elements/no-encontrado.png" /> 
+                        }
+                        </>
+                    )
+                } )}
+                        </Col>
+                    </Col>
+            </Row>
+            <Row>
+                <Col>
+                <Card.Body>
+                    <Card.Text>
+                    Some quick example text to build on the card title and make up the
+                    bulk of the card's content.
                     </Card.Text>
-                )
-            } )}
-        <Card.Text>Type: {type}</Card.Text>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
+                    <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+                </Col>
+            </Row>
+        </Container>
+        
     </Card>
     </>
 

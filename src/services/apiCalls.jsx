090c5b1cab0fb632715c4c827bdd5436f65a7ba9
Álvaro Fernández <126393419+Roekan@users.noreply.github.com>
2023-08-16 18:40:58 +0200

    Update apiCalls.jsx

--- src/services/apiCalls.jsx
+++ src/services/apiCalls.jsx
@@ -32,6 +32,10 @@ export const user = async (id) => {
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Headers': '*',
         'Access-Control-Allow-Methods': '*'
+        'Access-Control-Allow-Credentials", "value": "true" },
+    'Access-Control-Allow-Origin': '' },
+    'Access-Control-Allow-Methods': 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+    'Access-Control-Allow-Headers': 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' }
     }});
     return data;
 }
